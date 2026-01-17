770f39548fc61a23b9cfa98f1b8e86ded5d51e08
Stanislav Atanasov <stanislav.atan@gmail.com>
2026-01-17 11:44:48 +0200

    [feat] add tmuxp to installer and create ops layout

--- install.sh
+++ install.sh
@@ -66,7 +66,7 @@ create_symlink "$TMUX_DIR/.tmux.conf" "$HOME/.tmux.conf"
 
 echo "Installing Packages & Plugins..."
 
-PACKAGES="bat zoxide lazygit fzf starship fd visidata jq btop duckdb"
+PACKAGES="bat zoxide lazygit fzf starship fd visidata jq btop duckdb tmuxp"
 
 install_macos() {
     echo "   Detected macOS. Using Homebrew..."
